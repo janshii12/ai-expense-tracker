127679b380325e7e0c0509c79ba2898ea90ee690
janshii12 <janshiii1211@gmail.com>
2026-07-17 21:06:03 +0530

    Fix frontend-backend connection for production

--- server/main.py
+++ server/main.py
@@ -9,7 +9,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-expense-tracker-one-opal.vercel.app/"],
+    allow_origins=["https://localhost:5173", "https://ai-expense-tracker-one-opal.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
